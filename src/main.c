514d142f18c5d89532844fb4b41d4fc63d5d2ac4
j-a-r-i <jojanen@elisanet.fi>
2014-08-31 19:39:18 +0300

    early version of exti_init and rtc_init

--- src/main.c
+++ src/main.c
@@ -25,7 +25,39 @@ void stlinky_init(void)
 
 void exti_init(void)
 {
-	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource7);
+	SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+	EXTI_InitTypeDef extiInit;
+	EXTI_StructInit(&extiInit);
+	extiInit.EXTI_Line = EXTI_Line0;
+	extiInit.EXTI_Mode = EXTI_Mode_Interrupt;
+	extiInit.EXTI_Trigger = EXTI_Trigger_Rising;
+	extiInit.EXTI_LineCmd = ENABLE;
+	EXTI_Init(&extiInit);
+
+	NVIC_InitTypeDef nvicInit;
+	nvicInit.NVIC_IRQChannel = EXTI0_1_IRQn;
+	nvicInit.NVIC_IRQChannelPriority = 0x01;
+	nvicInit.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&nvicInit);
+}
+
+void rtc_init(void)
+{
+	RTC_InitTypeDef initRtc;
+    RTC_TimeTypeDef timeRtc;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR,ENABLE);
+
+	PWR_BackupAccessCmd(ENABLE);
+
+	RCC_BackupResetCmd(ENABLE);
+    RCC_BackupResetCmd(DISABLE);
+
+	//RCC_LSEConfig(RCC_LSI_ON);
+
+	//while (RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET);
+
+	RCC_RTCCLKConfig(RCC_RTCCLKSource_LSI);
 }
 
 int main(void)
