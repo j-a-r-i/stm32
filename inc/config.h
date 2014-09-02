59deea36a05df9ec26b6911a6905c2c30a442b20
j-a-r-i <jojanen@elisanet.fi>
2014-09-02 22:58:11 +0300

    new pin config

--- inc/config.h
+++ inc/config.h
@@ -2,21 +2,21 @@
 #define _CONFIG_H
 
 extern void config_port_init (void);
-#define set_LCD_DATA1 GPIOB->BSRR = GPIO_Pin_0
-#define clr_LCD_DATA1 GPIOB->BRR  = GPIO_Pin_0
-#define set_LCD_DATA2 GPIOB->BSRR = GPIO_Pin_1
-#define clr_LCD_DATA2 GPIOB->BRR  = GPIO_Pin_1
-#define set_LCD_DATA3 GPIOB->BSRR = GPIO_Pin_2
-#define clr_LCD_DATA3 GPIOB->BRR  = GPIO_Pin_2
-#define set_LCD_DATA4 GPIOB->BSRR = GPIO_Pin_3
-#define clr_LCD_DATA4 GPIOB->BRR  = GPIO_Pin_3
-#define set_LCD_RS GPIOB->BSRR = GPIO_Pin_4
-#define clr_LCD_RS GPIOB->BRR  = GPIO_Pin_4
-#define set_LCD_ENABLE GPIOB->BSRR = GPIO_Pin_5
-#define clr_LCD_ENABLE GPIOB->BRR  = GPIO_Pin_5
+#define set_LCD_DATA1 GPIOC->BSRR = GPIO_Pin_0
+#define clr_LCD_DATA1 GPIOC->BRR  = GPIO_Pin_0
+#define set_LCD_DATA2 GPIOC->BSRR = GPIO_Pin_1
+#define clr_LCD_DATA2 GPIOC->BRR  = GPIO_Pin_1
+#define set_LCD_DATA3 GPIOC->BSRR = GPIO_Pin_2
+#define clr_LCD_DATA3 GPIOC->BRR  = GPIO_Pin_2
+#define set_LCD_DATA4 GPIOC->BSRR = GPIO_Pin_3
+#define clr_LCD_DATA4 GPIOC->BRR  = GPIO_Pin_3
 #define set_LED1 GPIOC->BSRR = GPIO_Pin_8
 #define clr_LED1 GPIOC->BRR  = GPIO_Pin_8
 #define set_LED2 GPIOC->BSRR = GPIO_Pin_9
 #define clr_LED2 GPIOC->BRR  = GPIO_Pin_9
+#define set_LCD_RS GPIOC->BSRR = GPIO_Pin_10
+#define clr_LCD_RS GPIOC->BRR  = GPIO_Pin_10
+#define set_LCD_ENABLE GPIOC->BSRR = GPIO_Pin_11
+#define clr_LCD_ENABLE GPIOC->BRR  = GPIO_Pin_11
 
 #endif
--- src/config.c
+++ src/config.c
@@ -14,18 +14,29 @@ void config_port_init (void)
 	ioInit.GPIO_Speed = GPIO_Speed_10MHz;
 	GPIO_Init(GPIOA, &ioInit);
 	
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
+	ioInit.GPIO_Pin = GPIO_Pin_14 | GPIO_Pin_15;
+	ioInit.GPIO_Mode = GPIO_Mode_AF;
+	ioInit.GPIO_Speed = GPIO_Speed_10MHz;
+	GPIO_Init(GPIOA, &ioInit);
 	
-	ioInit.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5;
-	ioInit.GPIO_Mode = GPIO_Mode_OUT;
-	ioInit.GPIO_OType = GPIO_OType_PP;
-	ioInit.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource14, 0);
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, 0);
+	
+	ioInit.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+	ioInit.GPIO_Mode = GPIO_Mode_AF;
 	ioInit.GPIO_Speed = GPIO_Speed_10MHz;
 	GPIO_Init(GPIOB, &ioInit);
 	
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource3, 0);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource4, 0);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource5, 0);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource13, 0);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource14, 0);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource15, 0);
+	
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
 	
-	ioInit.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
+	ioInit.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10 | GPIO_Pin_11;
 	ioInit.GPIO_Mode = GPIO_Mode_OUT;
 	ioInit.GPIO_OType = GPIO_OType_PP;
 	ioInit.GPIO_PuPd = GPIO_PuPd_NOPULL;
