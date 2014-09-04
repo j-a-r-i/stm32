fd618348ff9de928895d141f6f2735e8160cce53
j-a-r-i <jojanen@elisanet.fi>
2014-09-04 22:27:13 +0300

    better RTC implementation and sending number to serial port

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "stm32f0xx.h"
 //#include "stlinky.h"
 #include "config.h"
+//#include <stdio.h>
 
 typedef void (*callback_fn)(void);
 
@@ -13,6 +14,8 @@ typedef void (*callback_fn)(void);
 static __IO uint32_t delayVar;
 static callback_fn timerCb;
 static uint32_t    timerCounter;
+static uint8_t     tmp;
+static RTC_TimeTypeDef timeRtc;
 
 void delay (int a);
 //void stlinky_init(void);
@@ -53,20 +56,53 @@ void exti_init(void)
 void rtc_init(void)
 {
 	RTC_InitTypeDef initRtc;
-    RTC_TimeTypeDef timeRtc;
+	RTC_AlarmTypeDef alarmRtc;
 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR,ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
 
 	PWR_BackupAccessCmd(ENABLE);
 
 	RCC_BackupResetCmd(ENABLE);
     RCC_BackupResetCmd(DISABLE);
 
-	//RCC_LSEConfig(RCC_LSI_ON);
+	RCC_LSICmd(ENABLE);
 
-	//while (RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET);
+	while (RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET);
 
 	RCC_RTCCLKConfig(RCC_RTCCLKSource_LSI);
+
+
+	initRtc.RTC_AsynchPrediv = 0x18f;
+	initRtc.RTC_SynchPrediv  = 0x63;
+	initRtc.RTC_HourFormat  = RTC_HourFormat_24;
+	if (RTC_Init(&initRtc) == ERROR)
+		usart_str("E RTC 1\r\n");
+
+	// Sets time
+	//
+	timeRtc.RTC_H12  = RTC_H12_AM;
+	timeRtc.RTC_Hours = 12;
+	timeRtc.RTC_Minutes = 00;
+	timeRtc.RTC_Seconds = 00;
+	if (RTC_SetTime(RTC_Format_BIN, &timeRtc))
+		usart_str("E RTC 2\r\n");
+
+	// Configure alarm
+	//
+	RTC_AlarmCmd(RTC_Alarm_A, DISABLE);
+	
+	alarmRtc.RTC_AlarmTime.RTC_H12 = RTC_H12_AM;
+	alarmRtc.RTC_AlarmTime.RTC_Hours = 12;
+	alarmRtc.RTC_AlarmTime.RTC_Minutes = 00;
+	alarmRtc.RTC_AlarmTime.RTC_Seconds = 20;
+	alarmRtc.RTC_AlarmDateWeekDay = 0x31;
+	alarmRtc.RTC_AlarmDateWeekDaySel = RTC_AlarmDateWeekDaySel_Date;
+	alarmRtc.RTC_AlarmMask = RTC_AlarmMask_DateWeekDay;
+	RTC_SetAlarm(RTC_Format_BIN, RTC_Alarm_A, &alarmRtc);
+
+	RTC_ITConfig(RTC_IT_ALRA, ENABLE);
+
+	RTC_AlarmCmd(RTC_Alarm_A, ENABLE);
 }
 
 void usart_init(void)
@@ -98,8 +134,7 @@ void usart_put(uint8_t ch)
 {
       USART_SendData(USART1, (uint8_t) ch);
 
-      while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET) {
-      }
+      while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
 }
 
 void usart_str(const char *s)
@@ -111,27 +146,53 @@ void usart_str(const char *s)
 	}
 }
 
+void usart_num(uint8_t n)
+{
+	char digit0, digit1, digit2;
+
+	digit0 = '0'+(n % 10);
+	n /= 10;
+	digit1 = '0'+(n % 10);
+	n /= 10;
+	digit2 = '0'+(n % 10);
+
+	usart_put(digit2);
+	usart_put(digit1);
+	usart_put(digit0);
+}
+
 int main(void)
 {
 	delayVar = 2000;
 	timerCounter = 0;
-
+	tmp = 0;
 	//	stlinky_init();
 
 	config_port_init();
 	usart_init();
+	rtc_init();
 
 	if (SysTick_Config(SystemCoreClock / 1000)) { 
 		/* Capture error */ 
 		while (1);
 	}
 
-	usart_str("\nHello\n");
+	usart_str("\r\nHello\r\n");
+	//printf("Hello\n");
 
 	//	stlinky_tx(&sterm, "Hi\n", 3);
 	while (1) {
 		//stlinky_tx(&sterm, "Hi", 2);
 	}
+}
+
+int __io_putchar(int ch)
+{
+	USART_SendData(USART1, (uint8_t) ch);
+
+	while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+
+	return ch;
 }
 
 void delay (int a)
@@ -159,6 +220,25 @@ void SysTick_Handler(void)
 	else if (delayVar == 0) {
 		clr_LED2;
 		delayVar = 2000;
-		USART_SendData(USART1, 'T');
+		//USART_SendData(USART1, 'T');
+		tmp++;
+
+		RTC_GetTime(RTC_Format_BIN, &timeRtc);
+		//usart_num(timeRtc.RTC_Hours);
+		usart_num(tmp);
+		usart_str(":");
+		usart_num(timeRtc.RTC_Minutes);
+		usart_str(":");
+		usart_num(timeRtc.RTC_Seconds);
+		usart_str("\r\n");
+	}
+}
+
+void RTC_IRQHandler(void)
+{
+	set_LED1;
+	if(RTC_GetITStatus(RTC_IT_ALRA) != RESET) {
+		USART_SendData(USART1, 'a');
+		RTC_ClearITPendingBit(RTC_IT_ALRA);
 	}
 }
